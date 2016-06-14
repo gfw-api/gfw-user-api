d56c99639a745404c06bec677819094f6c79b959
rrequero <rareq1987@gmail.com>
2016-06-14 11:46:07 +0200

    add staging config

--- app/src/app.js
+++ app/src/app.js
@@ -12,7 +12,7 @@ var loader = require('loader');
 var validate = require('koa-validate');
 var mongoose = require('mongoose');
 var ErrorSerializer = require('serializers/errorSerializer');
-var mongoUri = process.env.MONGOLAB_URI || 'mongodb://' + config.get('mongodb.host') + ':' + config.get('mongodb.port') + '/' + config.get('mongodb.database');
+var mongoUri = process.env.MONGO_URI || 'mongodb://' + config.get('mongodb.host') + ':' + config.get('mongodb.port') + '/' + config.get('mongodb.database');
 
 var onDbReady = function(err) {
     if (err) {
