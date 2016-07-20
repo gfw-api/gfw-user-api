82dc24f7021ea2bd669e18a1771422096c3229d2
rrequero <rareq1987@gmail.com>
2016-07-20 18:58:33 +0200

    add migrate

--- app/src/models/user.js
+++ app/src/models/user.js
@@ -4,6 +4,7 @@ var Schema = mongoose.Schema;
 
 var User = new Schema({
     fullName: {type: String, required:false, trim: true},
+    oldId: {type: Number, required: false},
     provider: {type: String, required:true, trim: true},
     providerId: {type: String, required:true, trim: true},
     email: {type: String, required:false, trim: true},
