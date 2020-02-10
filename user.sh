432e94e608d46967bf4d61a65698688bc19053ba
Tiago Garcia <tiagojsag@gmail.com>
2020-02-10 12:56:57 +0100

    Update dev dependencies; replace npm with yarn; update to node 12; eslint settings

--- user.sh
+++ user.sh
@@ -2,13 +2,13 @@
 
 case "$1" in
     test-e2e)
-        npm run test-e2e
+        yarn run test-e2e
         ;;
     test-unit)
-        npm run test-unit
+        yarn run test-unit
         ;;
     start)
-        npm start
+        yarn start
         ;;
     develop)
         type docker-compose >/dev/null 2>&1 || { echo >&2 "docker-compose is required but it's not installed.  Aborting."; exit 1; }
@@ -19,7 +19,7 @@ case "$1" in
         docker-compose -f docker-compose-test.yml run test
         ;;
   *)
-        echo "Usage: geostore.sh {test-e2e|test-unit|start|develop|test}" >&2
+        echo "Usage: user.sh {test-e2e|test-unit|start|develop|test}" >&2
         exit 1
         ;;
 esac
