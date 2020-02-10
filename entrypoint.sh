061ef7b2f2081aff248b8666fcf44436f6bb6bcc
Tiago Garcia <tiagojsag@gmail.com>
2020-02-10 13:29:24 +0100

    Improvements to docker setup and travis+codeclimate config

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,9 @@ case "$1" in
         echo "Running Development Server"
         exec grunt --gruntfile app/Gruntfile.js | bunyan
         ;;
-    startDev)
-        echo "Running Start Dev"
-        exec node app/index
-        ;;
     test)
         echo "Running Test"
-        exec grunt --gruntfile app/Gruntfile.js test
+        exec yarn test
         ;;
     start)
         echo "Running Start"
--- user.sh
+++ user.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 case "$1" in
-    test-e2e)
-        yarn run test-e2e
-        ;;
-    test-unit)
-        yarn run test-unit
-        ;;
     start)
         yarn start
         ;;
@@ -16,10 +10,10 @@ case "$1" in
         ;;
     test)
         type docker-compose >/dev/null 2>&1 || { echo >&2 "docker-compose is required but it's not installed.  Aborting."; exit 1; }
-        docker-compose -f docker-compose-test.yml run test
+        docker-compose -f docker-compose-test.yml build && docker-compose -f docker-compose-test.yml up --abort-on-container-exit
         ;;
   *)
-        echo "Usage: user.sh {test-e2e|test-unit|start|develop|test}" >&2
+        echo "Usage: user.sh {start|develop|test}" >&2
         exit 1
         ;;
 esac
