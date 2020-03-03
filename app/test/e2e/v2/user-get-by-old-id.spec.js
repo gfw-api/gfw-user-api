5579831265e965bf923d3a3c95959d398bd5ec87
Marta Gajowczyk <gajowczyk.marta@gmail.com>
2020-03-03 16:02:31 +0000

    Fix user-get-by-old-id V2 specs

--- app/test/e2e/v2/user-get-by-old-id.spec.js
+++ app/test/e2e/v2/user-get-by-old-id.spec.js
@@ -2,10 +2,10 @@
 const nock = require('nock');
 const chai = require('chai');
 const mongoose = require('mongoose');
-const UserModel = require('models/user');
+const UserModel = require('models/userV2');
 const { USERS } = require('../utils/test.constants');
 const { getTestServer } = require('../utils/test-server');
-const { createUser } = require('../utils/helpers');
+const { createUserV2 } = require('../utils/helpers');
 
 chai.use(require('chai-datetime'));
 
@@ -31,7 +31,7 @@ describe('V2 - Get user by old id tests', () => {
     });
 
     it('Get user by old id without being authenticated should return a 401 \'Unauthorized\' error', async () => {
-        const user = await new UserModel(createUser()).save();
+        const user = await new UserModel(createUserV2()).save();
 
         const response = await requester
             .get(`/api/v2/user/oldId/${user._id.toString()}`);
@@ -45,7 +45,7 @@ describe('V2 - Get user by old id tests', () => {
     it('Get user by old id while being authenticated as a different should return a 403 \'Forbidden\' error', async () => {
         const oldId = 12345;
 
-        await new UserModel(createUser({
+        await new UserModel(createUserV2({
             oldId
         })).save();
 
@@ -64,7 +64,7 @@ describe('V2 - Get user by old id tests', () => {
     it('Get user by old id while being authenticated as the same user should return a 200 and the user data (happy case)', async () => {
         const oldId = 12345;
 
-        const user = await new UserModel(createUser({
+        const user = await new UserModel(createUserV2({
             oldId
         })).save();
 
@@ -82,25 +82,22 @@ describe('V2 - Get user by old id tests', () => {
         response.body.data.should.have.property('type').and.equal('user');
         response.body.data.should.have.property('id').and.equal(user._id.toString());
         response.body.data.should.have.property('attributes').and.be.an('object');
-        response.body.data.attributes.should.have.property('fullName').and.equal(user.fullName);
+        response.body.data.attributes.should.have.property('firstName').and.equal(user.firstName);
+        response.body.data.attributes.should.have.property('lastName').and.equal(user.lastName);
         response.body.data.attributes.should.have.property('email').and.equal(user.email);
         response.body.data.attributes.should.have.property('createdAt');
         new Date(response.body.data.attributes.createdAt).should.equalDate(user.createdAt);
         response.body.data.attributes.should.have.property('sector').and.equal(user.sector);
-        response.body.data.attributes.should.have.property('primaryResponsibilities').and.include.members(user.primaryResponsibilities);
         response.body.data.attributes.should.have.property('country').and.equal(user.country);
         response.body.data.attributes.should.have.property('state').and.equal(user.state);
         response.body.data.attributes.should.have.property('city').and.equal(user.city);
         response.body.data.attributes.should.have.property('howDoYouUse').and.include.members(user.howDoYouUse);
-        response.body.data.attributes.should.have.property('signUpForTesting').and.equal(user.signUpForTesting);
-        response.body.data.attributes.should.have.property('language').and.equal(user.language);
-        response.body.data.attributes.should.have.property('profileComplete').and.equal(user.profileComplete);
     });
 
     it('Get user by old id while being authenticated as an ADMIN user should return a 200 and the user data (happy case)', async () => {
         const oldId = 12345;
 
-        const user = await new UserModel(createUser({
+        const user = await new UserModel(createUserV2({
             oldId
         })).save();
 
@@ -115,25 +112,22 @@ describe('V2 - Get user by old id tests', () => {
         response.body.data.should.have.property('type').and.equal('user');
         response.body.data.should.have.property('id').and.equal(user._id.toString());
         response.body.data.should.have.property('attributes').and.be.an('object');
-        response.body.data.attributes.should.have.property('fullName').and.equal(user.fullName);
+        response.body.data.attributes.should.have.property('firstName').and.equal(user.firstName);
+        response.body.data.attributes.should.have.property('lastName').and.equal(user.lastName);
         response.body.data.attributes.should.have.property('email').and.equal(user.email);
         response.body.data.attributes.should.have.property('createdAt');
         new Date(response.body.data.attributes.createdAt).should.equalDate(user.createdAt);
         response.body.data.attributes.should.have.property('sector').and.equal(user.sector);
-        response.body.data.attributes.should.have.property('primaryResponsibilities').and.include.members(user.primaryResponsibilities);
         response.body.data.attributes.should.have.property('country').and.equal(user.country);
         response.body.data.attributes.should.have.property('state').and.equal(user.state);
         response.body.data.attributes.should.have.property('city').and.equal(user.city);
         response.body.data.attributes.should.have.property('howDoYouUse').and.include.members(user.howDoYouUse);
-        response.body.data.attributes.should.have.property('signUpForTesting').and.equal(user.signUpForTesting);
-        response.body.data.attributes.should.have.property('language').and.equal(user.language);
-        response.body.data.attributes.should.have.property('profileComplete').and.equal(user.profileComplete);
     });
 
     it('Get user by old id while being authenticated as an MICROSERVICE user should return a 200 and the user data (happy case)', async () => {
         const oldId = 12345;
 
-        const user = await new UserModel(createUser({
+        const user = await new UserModel(createUserV2({
             oldId
         })).save();
 
@@ -148,19 +142,16 @@ describe('V2 - Get user by old id tests', () => {
         response.body.data.should.have.property('type').and.equal('user');
         response.body.data.should.have.property('id').and.equal(user._id.toString());
         response.body.data.should.have.property('attributes').and.be.an('object');
-        response.body.data.attributes.should.have.property('fullName').and.equal(user.fullName);
+        response.body.data.attributes.should.have.property('firstName').and.equal(user.firstName);
+        response.body.data.attributes.should.have.property('lastName').and.equal(user.lastName);
         response.body.data.attributes.should.have.property('email').and.equal(user.email);
         response.body.data.attributes.should.have.property('createdAt');
         new Date(response.body.data.attributes.createdAt).should.equalDate(user.createdAt);
         response.body.data.attributes.should.have.property('sector').and.equal(user.sector);
-        response.body.data.attributes.should.have.property('primaryResponsibilities').and.include.members(user.primaryResponsibilities);
         response.body.data.attributes.should.have.property('country').and.equal(user.country);
         response.body.data.attributes.should.have.property('state').and.equal(user.state);
         response.body.data.attributes.should.have.property('city').and.equal(user.city);
         response.body.data.attributes.should.have.property('howDoYouUse').and.include.members(user.howDoYouUse);
-        response.body.data.attributes.should.have.property('signUpForTesting').and.equal(user.signUpForTesting);
-        response.body.data.attributes.should.have.property('language').and.equal(user.language);
-        response.body.data.attributes.should.have.property('profileComplete').and.equal(user.profileComplete);
     });
 
     it('Get user by old id for an invalid id should return a 404 \'User not found\' error', async () => {
