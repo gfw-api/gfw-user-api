c6763f8d6eb09164ed4f42ddb4b1a3291d21b9ad
Solomon Negusse <solomon.negusse@wri.org>
2024-12-06 13:11:19 +0300

    update docker-compose -> docker compose

--- user.sh
+++ user.sh
@@ -5,14 +5,14 @@ case "$1" in
         yarn start
         ;;
     develop)
-        type docker-compose >/dev/null 2>&1 || { echo >&2 "docker-compose is required but it's not installed.  Aborting."; exit 1; }
-        docker-compose -f docker-compose-develop.yml build && docker-compose -f docker-compose-develop.yml up
+        type docker compose >/dev/null 2>&1 || { echo >&2 "docker compose is required but it's not installed.  Aborting."; exit 1; }
+        docker compose -f docker-compose-develop.yml build && docker compose -f docker-compose-develop.yml up
         ;;
     test)
-        type docker-compose >/dev/null 2>&1 || { echo >&2 "docker-compose is required but it's not installed.  Aborting."; exit 1; }
-        docker-compose -f docker-compose-test.yml build && docker-compose -f docker-compose-test.yml up --abort-on-container-exit
+        type docker compose >/dev/null 2>&1 || { echo >&2 "docker compose is required but it's not installed.  Aborting."; exit 1; }
+        docker compose -f docker-compose-test.yml build && docker compose -f docker-compose-test.yml up --abort-on-container-exit
         ;;
-  *)
+    *)
         echo "Usage: user.sh {start|develop|test}" >&2
         exit 1
         ;;
